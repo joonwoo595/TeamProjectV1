b88776e166844bee7ecb9325025784e8f8559bad
joonwoo595 <jonbeo312@gmail.com>
2025-02-27 14:52:56 +0900

    준우에 첫 커밋

---
+++ src/main/java/project/duo/teamprojectduo/TeamProjectDuoApplication.java
@@ -0,0 +1,13 @@
+package project.duo.teamprojectduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectDuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectDuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/duo/teamprojectduo/TeamProjectDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.duo.teamprojectduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectDuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
